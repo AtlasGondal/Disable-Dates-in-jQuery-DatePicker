eb7c58762ae2cc5c711a0394d0fa82b1b13a278e
Atlas Gondal <Atlas_Gondal@Yahoo.com>
2015-12-19 16:44:49 +0500

    initial commit

---
+++ DisableParticularDates.js
@@ -0,0 +1,21 @@
+function DisableMonday(date) {
+ 
+  var day = date.getDay();
+  // If day == 1 then it is MOnday
+  if (day == 1) {
+   
+    return [false] ; 
+    
+  } else { 
+   
+    return [true] ;
+   
+   }
+  
+}
+  
+ $(function() {
+    $( "#datepicker" ).datepicker({
+        beforeShowDay: DisableMonday
+    });
+ });
